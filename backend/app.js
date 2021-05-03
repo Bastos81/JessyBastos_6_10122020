cee4c9cbef31f5c84fd4d62001c812ce651ae81f
Jessy Bastos <jessybastos.web@gmail.com>
2021-05-03 09:47:53 +0200

    Router

--- backend/app.js
+++ backend/app.js
@@ -22,38 +22,5 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.post('/api/sauce', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-      ...req.body
-    });
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-      .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/sauce/:id', (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-      .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/sauce/:id', (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-app.get('/api/sauce/:id', (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-      .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/sauce', (req, res, next) => {
-    Sauce.find()
-      .then(sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({ error }));
-});
 
 module.exports = app;
